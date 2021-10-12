0afa05f1ce3cbce19bfd1e22d3e2e7a5307bed24
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2021-10-12 16:10:47 +0200

    Add files via upload
    Initial

---
+++ 01.data/A_DIM_ACTIVITY.sql
@@ -0,0 +1,45 @@
+/****** Object:  Table [dbo].[A_DIM_ACTIVITY]    Script Date: 12-10-2021 13:46:43 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE S_1_W.[A_DIM_ACTIVITY](
+	[activity_id] [int] IDENTITY(1,1) NOT NULL,
+	[activity_name] [nvarchar](150) NOT NULL,
+	[activity_set] [nvarchar](250) NULL,
+	[activity_code] [nvarchar](150) NULL,
+	[description] [nvarchar](4000) NULL,
+	[segment] [nvarchar](250) NULL,
+	[domain] [nvarchar](250) NULL,
+	[region] [nvarchar](250) NULL,
+	[template_id] [int] NULL,
+	[slicer1] [nvarchar](150) NULL,
+	[slicer2] [nvarchar](150) NULL,
+	[slicer3] [nvarchar](150) NULL,
+	[sort_order] [tinyint] NULL,
+	[resource] [nvarchar](250) NULL,
+	[channel] [nvarchar](250) NULL,
+	[reference] [nvarchar](250) NULL,
+	[parent] [nvarchar](250) NULL,
+	[status] [nvarchar](150) NULL,
+	[plantype] [nvarchar](150) NULL,
+	[category] [nvarchar](150) NULL,
+	[site_id] [int] NULL,
+	[activity_guid] [nvarchar](40) NULL,
+	[active] [bit] NULL,
+	[tags] [nvarchar](1000) NULL,
+	[color] [nvarchar](50) NULL,
+	[font_awesome] [nvarchar](550) NULL,
+	[date_updated] [datetime] NULL,
+	[date_created] [datetime] NULL,
+	[timestamp] [timestamp] NULL,
+ CONSTRAINT [PK_A_DIM_ACTIVITY_NEW] PRIMARY KEY CLUSTERED 
+(
+	[activity_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
