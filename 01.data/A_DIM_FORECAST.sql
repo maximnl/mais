7be71611d0bbce4d6491ee9c2a9940cd8d436ca4
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2021-11-03 15:34:23 +0100

    Update A_DIM_FORECAST.sql

--- 01.data/A_DIM_FORECAST.sql
+++ 01.data/A_DIM_FORECAST.sql
@@ -1,11 +1,11 @@
-/****** Object:  Table [dbo].[A_DIM_FORECAST]    Script Date: 12-10-2021 16:11:31 ******/
+/****** Object:  Table [S_1_W].[A_DIM_FORECAST]    Script Date: 3-11-2021 15:32:23 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[A_DIM_FORECAST](
+CREATE TABLE dbo.[A_DIM_FORECAST](
 	[forecast_id] [int] IDENTITY(1,1) NOT NULL,
 	[forecast_name] [nvarchar](150) NOT NULL,
 	[forecast_code] [nvarchar](50) NULL,
@@ -17,8 +17,6 @@ CREATE TABLE [dbo].[A_DIM_FORECAST](
 	[active] [bit] NULL,
 	[sort_order] [int] NULL,
 	[site_id] [int] NULL,
-	[date_valid_from] [datetime] NULL,
-	[date_valid_until] [datetime] NULL,
 	[date_updated] [datetime] NULL,
 	[date_created] [datetime] NULL,
 	[timestamp] [timestamp] NULL,
@@ -27,6 +25,15 @@ CREATE TABLE [dbo].[A_DIM_FORECAST](
 	[forecast_id] ASC
 )WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE dbo.[A_DIM_FORECAST] ADD  CONSTRAINT [DF_A_DIM_FORECAST_forecast_guid]  DEFAULT (newid()) FOR [forecast_guid]
+GO
+
+ALTER TABLE dbo.[A_DIM_FORECAST] ADD  CONSTRAINT [DF_A_DIM_FORECAST_active]  DEFAULT ((1)) FOR [active]
+GO
+
+ALTER TABLE dbo.[A_DIM_FORECAST] ADD  CONSTRAINT [DF_A_DIM_FORECAST_site_id]  DEFAULT ((1)) FOR [site_id]
 GO
 
 
