1b65956193aa6351479aa7a404f2b129392d28bb
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2021-12-16 12:34:42 +0100

    Update A_IMPORT.sql

--- 01.data/A_IMPORT.sql
+++ 01.data/A_IMPORT.sql
@@ -1,3 +1,4 @@
+/****** Object:  Table [dbo].[A_IMPORT]    Script Date: 16-12-2021 12:33:39 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,29 +11,35 @@ CREATE TABLE [dbo].[A_IMPORT](
 	[domain] [nvarchar](50) NULL,
 	[procedure_id] [int] NULL,
 	[procedure_code] [nvarchar](150) NULL,
-	[import_guid] [nvarchar](40) NULL,
 	[activity_id] [int] NULL,
 	[forecast_id] [int] NULL,
+	[source] [nvarchar](2000) NULL,
+	[filter] [nvarchar](2000) NULL,
+	[fields_source] [nvarchar](2000) NULL,
+	[fields_target] [nvarchar](2000) NULL,
+	[group_by] [nvarchar](2000) NULL,
+	[category] [nvarchar](150) NULL,
+	[status] [nvarchar](150) NULL,
+	[version] [nvarchar](50) NULL,
+	[commands] [nvarchar](500) NULL,
 	[p1] [nvarchar](2000) NULL,
 	[p2] [nvarchar](2000) NULL,
 	[p3] [nvarchar](2000) NULL,
 	[p4] [nvarchar](2000) NULL,
 	[p5] [nvarchar](2000) NULL,
 	[schedule] [nvarchar](400) NULL,
+	[days_back] [nvarchar](150) NULL,
+	[days_forward] [nvarchar](150) NULL,
 	[date_import_from] [datetime] NULL,
 	[date_import_until] [datetime] NULL,
 	[description] [nvarchar](2000) NULL,
 	[sort_order] [int] NULL,
 	[active] [bit] NULL,
-	[fields_source] [nvarchar](2000) NULL,
-	[fields_target] [nvarchar](2000) NULL,
-	[source] [nvarchar](2000) NULL,
-	[filter] [nvarchar](2000) NULL,
-	[group_by] [nvarchar](2000) NULL,
 	[template_id] [int] NULL,
 	[site_id] [int] NULL,
 	[date_updated] [datetime] NULL,
 	[date_created] [datetime] NULL,
+	[import_guid] [nvarchar](40) NULL,
 	[timestamp] [timestamp] NULL,
  CONSTRAINT [PK_A_STG_IMPORT] PRIMARY KEY CLUSTERED 
 (
@@ -41,19 +48,16 @@ CREATE TABLE [dbo].[A_IMPORT](
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[A_IMPORT] ADD  CONSTRAINT [DF_A_IMPORT_import_guid]  DEFAULT (newid()) FOR [import_guid]
-GO
-
 ALTER TABLE [dbo].[A_IMPORT] ADD  CONSTRAINT [DF_A_IMPORT_active]  DEFAULT ((1)) FOR [active]
-GO
-
-ALTER TABLE [dbo].[A_IMPORT] ADD  CONSTRAINT [DF_A_IMPORT_site_id]  DEFAULT ((1)) FOR [site_id]
 GO
 
 ALTER TABLE [dbo].[A_IMPORT] ADD  CONSTRAINT [DF_A_IMPORT_date_updated_1]  DEFAULT (getdate()) FOR [date_updated]
 GO
 
 ALTER TABLE [dbo].[A_IMPORT] ADD  CONSTRAINT [DF_A_IMPORT_date_created_1]  DEFAULT (getdate()) FOR [date_created]
+GO
+
+ALTER TABLE [dbo].[A_IMPORT] ADD  CONSTRAINT [DF_A_IMPORT_import_guid]  DEFAULT (newid()) FOR [import_guid]
 GO
 
 
