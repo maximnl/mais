ff745263a47e0f9e8d573cf26426141c9f8a3bc6
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2022-02-21 09:45:49 +0100

    Update A_FACT_DAY.sql
    value3-value10 are made sparse , to minimize table size

--- 01.data/A_FACT_DAY.sql
+++ 01.data/A_FACT_DAY.sql
@@ -1,31 +1,28 @@
-/****** Object:  Table [S_1_W].[A_FACT_DAY]    Script Date: 15-2-2022 10:46:51 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[A_FACT_DAY](
+CREATE TABLE [DBO].[A_FACT_DAY](
 	[date] [datetime] NOT NULL,
 	[activity_id] [int] NOT NULL,
 	[forecast_id] [int] NOT NULL,
 	[value1] [float] NULL,
 	[value2] [float] NULL,
 	[value3] [float] NULL,
-	[value4] [float] NULL,
-	[value5] [float] NULL,
-	[value6] [float] NULL,
-	[value7] [float] NULL,
-	[value8] [float] NULL,
-	[value9] [float] NULL,
-	[value10] [float] NULL,
-	[import_id] [int] NULL,
+	[value4] [float] SPARSE  NULL,
+	[value5] [float] SPARSE  NULL,
+	[value6] [float] SPARSE  NULL,
+	[value7] [float] SPARSE  NULL,
+	[value8] [float] SPARSE  NULL,
+	[value9] [float] SPARSE  NULL,
+	[value10] [float] SPARSE  NULL,
 	[site_id] [int] NULL,
-	[date_updated] [datetime] NULL
+	[import_id] [int] NULL,
+	[date_updated] [datetime] NULL,
 ) ON [PRIMARY]
-GO
 
 ALTER TABLE [S_1_W].[A_FACT_DAY] ADD  CONSTRAINT [DF_A_FACT_DAY_date_updated]  DEFAULT (getdate()) FOR [date_updated]
 GO
-
 
