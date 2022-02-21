74c00424452fd7a924cce9ada236baae487b5f33
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2022-02-21 10:38:39 +0100

    Update A_FACT_INTRADAY.sql

--- 01.data/A_FACT_INTRADAY.sql
+++ 01.data/A_FACT_INTRADAY.sql
@@ -1,22 +1,31 @@
+/****** Object:  Table [A_FACT_INTRADAY]    Script Date: 21-2-2022 10:34:50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
 
 CREATE TABLE [A_FACT_INTRADAY](
 	[date] [datetime] NULL,
 	[interval_id] [smallint] NULL,
 	[activity_id] [int] NULL,
 	[forecast_id] [int] NULL,
-	[value1] [float] NULL,
-	[value2] [float] NULL,
-	[value3] [float] NULL,
-	[value4] [float] SPARSE  NULL,
-	[value5] [float] SPARSE  NULL,
-	[value6] [float] SPARSE  NULL,
-	[value7] [float] SPARSE  NULL,
-	[value8] [float] SPARSE  NULL,
-	[value9] [float] SPARSE  NULL,
-	[value10] [float] SPARSE  NULL,
+	[value1] [real] NULL,
+	[value2] [real] NULL,
+	[value3] [real] NULL,
+	[value4] [real] SPARSE  NULL,
+	[value5] [real] SPARSE  NULL,
+	[value6] [real] SPARSE  NULL,
+	[value7] [real] SPARSE  NULL,
+	[value8] [real] SPARSE  NULL,
+	[value9] [real] SPARSE  NULL,
+	[value10] [real] SPARSE  NULL,
 	[duration_min] [tinyint] NULL,
 	[import_id] [int] NULL,
 	[date_updated] [datetime] NULL,
 	[site_id] [int] NULL
 ) ON [PRIMARY]
 GO
+
+
+
