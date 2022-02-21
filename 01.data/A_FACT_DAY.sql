ba877437a10c33d1856c8a47347624cf7c929f50
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2022-02-21 10:34:19 +0100

    Update A_FACT_DAY.sql
    float was set to real type, indexes added

--- 01.data/A_FACT_DAY.sql
+++ 01.data/A_FACT_DAY.sql
@@ -1,26 +1,49 @@
+/****** Object:  Table [A_FACT_DAY]    Script Date: 21-2-2022 10:02:54 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [A_FACT_DAY](
+CREATE TABLE .[A_FACT_DAY] (
 	[date] [datetime] NOT NULL,
 	[activity_id] [int] NOT NULL,
 	[forecast_id] [int] NOT NULL,
-	[value1] [float] NULL,
-	[value2] [float] NULL,
-	[value3] [float] NULL,
-	[value4] [float] SPARSE  NULL,
-	[value5] [float] SPARSE  NULL,
-	[value6] [float] SPARSE  NULL,
-	[value7] [float] SPARSE  NULL,
-	[value8] [float] SPARSE  NULL,
-	[value9] [float] SPARSE  NULL,
-	[value10] [float] SPARSE  NULL,
+	[value1] [real] NULL,
+	[value2] [real] NULL,
+	[value3] [real] NULL,
+	[value4] [real] SPARSE  NULL,
+	[value5] [real] SPARSE  NULL,
+	[value6] [real] SPARSE  NULL,
+	[value7] [real] SPARSE  NULL,
+	[value8] [real] SPARSE  NULL,
+	[value9] [real] SPARSE  NULL,
+	[value10] [real] SPARSE  NULL,
 	[site_id] [int] NULL,
 	[import_id] [int] NULL,
-	[date_updated] [datetime] NULL,
+	[date_updated] [datetime] NULL
 ) ON [PRIMARY]
+GO
+
+/****** Object:  Index [idx_fact_day_activity_id]    Script Date: 21-2-2022 10:02:54 ******/
+CREATE NONCLUSTERED INDEX [idx_fact_day_activity_id] ON .[A_FACT_DAY]
+(
+	[activity_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+
+/****** Object:  Index [idx_fact_day_date]    Script Date: 21-2-2022 10:02:54 ******/
+CREATE NONCLUSTERED INDEX [idx_fact_day_date] ON .[A_FACT_DAY]
+(
+	[date] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
+
+/****** Object:  Index [idx_fact_day_forecast_id]    Script Date: 21-2-2022 10:02:54 ******/
+CREATE NONCLUSTERED INDEX [idx_fact_day_forecast_id] ON .[A_FACT_DAY]
+(
+	[forecast_id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+GO
 
 
