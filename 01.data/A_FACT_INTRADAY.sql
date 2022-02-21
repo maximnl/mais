763e1f7cc742be3dc073e4471e38a8d4490b6eb0
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2022-02-21 09:50:47 +0100

    Update A_FACT_INTRADAY.sql

--- 01.data/A_FACT_INTRADAY.sql
+++ 01.data/A_FACT_INTRADAY.sql
@@ -1,11 +1,5 @@
-/******     Script Date: 15-2-2022 10:49:32 ******/
-SET ANSI_NULLS ON
-GO
 
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [DBO].[A_FACT_INTRADAY](
+CREATE TABLE [A_FACT_INTRADAY](
 	[date] [datetime] NULL,
 	[interval_id] [smallint] NULL,
 	[activity_id] [int] NULL,
@@ -13,17 +7,16 @@ CREATE TABLE [DBO].[A_FACT_INTRADAY](
 	[value1] [float] NULL,
 	[value2] [float] NULL,
 	[value3] [float] NULL,
-	[value4] [float] NULL,
-	[value5] [float] NULL,
-	[value6] [float] NULL,
-	[value7] [float] NULL,
-	[value8] [float] NULL,
-	[value9] [float] NULL,
-	[value10] [float] NULL,
+	[value4] [float] SPARSE  NULL,
+	[value5] [float] SPARSE  NULL,
+	[value6] [float] SPARSE  NULL,
+	[value7] [float] SPARSE  NULL,
+	[value8] [float] SPARSE  NULL,
+	[value9] [float] SPARSE  NULL,
+	[value10] [float] SPARSE  NULL,
 	[duration_min] [tinyint] NULL,
 	[import_id] [int] NULL,
 	[date_updated] [datetime] NULL,
 	[site_id] [int] NULL
 ) ON [PRIMARY]
 GO
- 
