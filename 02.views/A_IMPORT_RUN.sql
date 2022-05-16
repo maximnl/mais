6efafd0d89ae7ff38fb025c9722a6e26e9e4bc4b
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2022-05-16 16:57:34 +0200

    Update A_IMPORT_RUN.sql
    days back and days forward sync with delta

--- 02.views/A_IMPORT_RUN.sql
+++ 02.views/A_IMPORT_RUN.sql
@@ -1,21 +1,16 @@
-
-/****** Object:  View [dbo].[A_IMPORT_RUN]    Script Date: 18-1-2022 11:56:07 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-
-CREATE OR ALTER   VIEW [A_IMPORT_RUN] as 
+CREATE OR ALTER     VIEW [dbo].[A_IMPORT_RUN] as 
 SELECT  I.import_id
      	,case when isnull(I.[domain],'')>'' then I.[domain] else P.[domain] end as [domain]     
      	,P.[procedure_name]
 		,P.[procedure_code] 
 	  	,P.app
-	 	,trim(isnull(P.commands,''))+trim(isnull(I.commands,''))   as commands     
+        , P.[status]
+	 	,trim(isnull(P.commands,''))+trim(isnull(I.commands,'')) + ' -DELTA'   as commands     
      	,I.[activity_id]
      	,I.[forecast_id]
      	,case when isnull(I.[p1],'')>'' then I.[p1] else P.[p1] end as [p1]
@@ -25,9 +20,9 @@ SELECT  I.import_id
 		,case when isnull(I.[p5],'')>'' then I.[p5] else P.[p5] end as [p5]
      	,isnull(P.[sort_order],1) *1000+ isnull(I.[sort_order],0) sort_order
      	,'' as description
-     	,case when P.[days_back] >'' then dateadd(D,-1* case when isnull(I.[days_back],'')>'' then I.[days_back] else P.[days_back] end ,getdate()) else
+     	,case when P.[days_back]+I.[days_back] >'' then dateadd(D,-1* case when isnull(I.[days_back],'')>'' then I.[days_back] else P.[days_back] end ,getdate()) else
 			isnull(I.[date_import_from], '1900-01-01') end date_import_from
-     	,case when P.[days_forward] >'' then dateadd(D, 1 * case when isnull(I.[days_forward],'')>'' then I.[days_forward] else P.[days_forward] end,getdate()) else 
+     	,case when P.[days_forward]+I.[days_forward] >'' then dateadd(D, 1 * case when isnull(I.[days_forward],'')>'' then I.[days_forward] else P.[days_forward] end,getdate()) else 
 			isnull(I.[date_import_until],'9999-01-01') end date_import_until
      	,case when isnull(I.[fields_source],'') >'' then I.[fields_source] else P.fields_source end fields_source
 	  	,case when isnull(I.[fields_target],'') >'' then I.[fields_target] else P.fields_target end fields_target
@@ -40,6 +35,7 @@ SELECT  I.import_id
 	  	,case when isnull(I.[group_by],'')>'' then I.[group_by] else P.[group_by] end group_by
  		,A.activity_name
 		,A.activity_set
+        ,I.site_id
   FROM [A_IMPORT] I
 	  inner join [A_IMPORT_PROCEDURE] P on I.procedure_id=P.procedure_id
 	  inner join [A_DIM_ACTIVITY] A on I.activity_id=A.activity_id
@@ -48,5 +44,3 @@ SELECT  I.import_id
  
 
 GO
-
-
