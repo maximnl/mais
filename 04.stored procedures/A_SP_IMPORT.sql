7fd56b5f2ac3a9075d2c0212b43e07b7142f70c3
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2022-06-23 12:12:47 +0200

    Update A_SP_IMPORT.sql
    default day/intraday laden if source is empty

--- 04.stored procedures/A_SP_IMPORT.sql
+++ 04.stored procedures/A_SP_IMPORT.sql
@@ -1,41 +1,41 @@
-
-/****** Object:  StoredProcedure [dbo].[A_SP_IMPORT]    Script Date: 30-5-2022 16:35:42 ******/
 SET ANSI_NULLS OFF
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+-- version 20220622
+-- default day and intraday tables are used if the source parameter left empty.
 
+-- version 20220603
 -- generic import of transactional data into time series/ MAIS data format
 -- this SP will generate queries and run/print them for every row from [A_IMPORT_RUN] view
 -- template stored procedure for loading data from source tables
-CREATE OR ALTER   PROCEDURE [dbo].[A_SP_IMPORT]
+ALTER     PROCEDURE [dbo].[A_SP_IMPORT]
  @activity_id int = 0 
 ,@session_id uniqueidentifier   = null
 ,@commands varchar(2000)='' -- '-LOG_ROWCOUNT -LOG_INSERT -LOG_DELETE -PRINT -NOGROUPBY -SUMFIELDS -NOINTRADAY -NODELTA -INTRADAY
 ,@procedure_name nvarchar(200)='A_SP_IMPORT'
 ,@site_id int =0
 ,@import_id int =0
+,@fact_day nvarchar(200) ='[A_FACT_DAY]' -- data per day stored here
+,@fact_intraday nvarchar(200)='[A_FACT_INTRADAY]' -- data per day/interval_id is stored here. conform a_time_interval dimension
 AS
 BEGIN
-
     SET NOCOUNT ON;
-
 --  configuration
-	DECLARE @fact_day nvarchar(200)='[A_FACT_DAY]' -- data per day stored here
-	DECLARE @fact_intraday nvarchar(200)='[A_FACT_INTRADAY]' -- data per day/interval_id is stored here. conform a_time_interval dimension
-    DECLARE @sqlCommand NVARCHAR(MAX) -- 
+    DECLARE @sqlCommand NVARCHAR(MAX) -- 
 
 --  source data parameters
 	DECLARE @forecast_id int = 0
-	DECLARE @filter nvarchar(4000)='' -- where filter for filtering source data
+	DECLARE @filter nvarchar(4000)=''           -- where filter for filtering source data
 	DECLARE @date_import_from date='1900-01-01' -- calculated by the import query using imports and procedures fields
 	DECLARE @date_import_until date='9999-01-01'
-	DECLARE @fields_source varchar(2000)='' -- source fields
-	DECLARE @fields_target varchar(2000)=''  -- target fields value1
+	DECLARE @fields_source varchar(2000)=''     -- source fields
+	DECLARE @fields_target varchar(2000)=''     -- target fields value1
 	DECLARE @schedule varchar(2000)=''
 	DECLARE @source varchar(2000)=''
 	DECLARE @group_by varchar(2000)=''
-	DECLARE @p1 varchar(2000)=''  -- parameters
+    -- parameters
+	DECLARE @p1 varchar(2000)=''                
 	DECLARE @p2 varchar(2000)=''
 	DECLARE @p3 varchar(2000)=''
 	DECLARE @p4 varchar(2000)=''
@@ -51,13 +51,14 @@ BEGIN
 	-- source data analysis
 	DECLARE @date_source_min date='9999-01-01' -- calculated by the import query using imports and procedures fields
 	DECLARE @date_source_max date='1900-01-01'
+    DECLARE @day_source varchar(max)=''
 
 	-- intraday parameters
 	DECLARE @intraday_join varchar(2000)=''
 	DECLARE @intraday_interval_id varchar(200)='interval_id'
 	DECLARE @intraday_duration varchar(5)='15' -- default intraday interval duration in min
+    DECLARE @intraday_source varchar(max)=''
 
-    
 	DECLARE TAB_CURSOR CURSOR  FOR 
     SELECT import_id 
  	  ,[activity_id]
@@ -86,16 +87,12 @@ BEGIN
 	EXEC dbo.[A_SP_SYS_LOG] 'PROCEDURE START' ,@session_id  ,@activity_id  , @procedure_name ,@commands
 	SET @start_time     = GETDATE()
 
-	
-
 ----------------------------------------------
 --  FETCH ALL IMPORTS FOR THE CURRENT SP
 ----------------------------------------------
 	OPEN TAB_CURSOR 
-
 	FETCH NEXT FROM TAB_CURSOR 
-
-	INTO   @import_id 
+	INTO     @import_id 
  			,@activity_id
      		,@forecast_id
      		,@p1
@@ -114,23 +111,18 @@ BEGIN
 			,@commands
 			,@procedure_name
 			,@site_id 
-
 	WHILE @@FETCH_STATUS = 0 
-
-   		BEGIN 
-
-			IF @commands NOT like '%-NODELTA%' BEGIN 
-
+   	BEGIN 
+			IF @commands NOT like '%-NODELTA%' BEGIN
 				declare @dates varchar(30)
 				SET @sqlCommand = 'select @dates = convert(char(10),convert(date,isnull(min(' + @group_by + '),''9999-12-31'')),126) + convert(char(10),convert(date,isnull(max(' + @group_by + '),''1900-12-31'')),126)
 				 FROM ' + @source +' WHERE ' + @filter
 				EXEC sp_executesql @sqlCommand, N'@dates varchar(30) OUTPUT', @dates=@dates OUTPUT
-				set @date_source_min=left(@dates,10)
-				set @date_source_max=right(@dates,10)
+				SET @date_source_min=left(@dates,10)
+				SET @date_source_max=right(@dates,10)
 
-                IF @date_import_from<@date_source_min BEGIN set @date_import_from=@date_source_min END
-                IF @date_import_until>@date_source_max BEGIN set @date_import_until=@date_source_max END
-				 
+                IF @date_import_from<@date_source_min BEGIN SET @date_import_from=@date_source_min END
+                IF @date_import_until>@date_source_max BEGIN SET @date_import_until=@date_source_max END				 
 			END
 
 			IF @commands like '%-SUMFIELDS%' and @fields_source not like '%SUM(%)%' set @fields_source= concat('SUM(convert(float,',replace(@fields_source,',',')),SUM(convert(float,('),'))')
@@ -151,7 +143,7 @@ BEGIN
 	----------------------------------------------------------------------------------------------------------------------
 	--  CLEAN DAY
 	--------------------------------------------------------------------------------
- 		
+ 		SET @day_source= case when @source>'' then @source else @fact_day end
 		SET @sqlCommand = 
  		'DELETE FROM '+ @fact_day +' WHERE [date] BETWEEN ''' + convert(char(10),@date_import_from,126)  + ''' AND ''' + convert(char(10),@date_import_until,126) + ''' AND activity_id =' +  convert(nvarchar(max),@activity_id) 
 + '	AND forecast_id = ' +  convert(nvarchar(max),@forecast_id);
@@ -164,7 +156,8 @@ BEGIN
             BEGIN
                 EXEC( @sqlCommand)
                 SET @rows= @@ROWCOUNT          
-                IF @date_import_until<@date_import_from AND  @commands like '%-LOG_ROWCOUNT%' EXEC dbo.[A_SP_SYS_LOG]  'IMPORT WARNING' ,@session_id ,@import_id ,'NODATA ON DELETE', @sqlCommand  
+                IF @date_import_until<@date_import_from AND  @commands like '%-LOG_ROWCOUNT%' 
+                    EXEC dbo.[A_SP_SYS_LOG]  'IMPORT WARNING' ,@session_id ,@import_id ,'NODATA ON DELETE', @sqlCommand  
                 SET @output=@output+'day records deleted ' + convert(varchar(10),@rows)+'<br><br>'
                 IF @commands like '%-LOG_ROWCOUNT%' EXEC dbo.[A_SP_SYS_LOG] 'LOG ROWS' ,@session_id ,@import_id ,'RECORDS DELETE DAY',@rows  
                 IF @commands like '%-LOG_DELETE%' EXEC dbo.[A_SP_SYS_LOG] 'LOG DELETE ROWS' ,@session_id ,@import_id ,'DELETE QUERY DAY',@sqlCommand  
@@ -179,14 +172,16 @@ BEGIN
 	--------------------------------------------------------------------------------------------------------------------------
 	--  INSERT TO DAY
 	-------------------------------------------------------------------------------------
- 		IF @commands not like '%-NOGROUPBY%' OR @commands like  '%-SUMFIELDS%' BEGIN SET @groupby=concat(' GROUP BY ',@group_by) END ELSE SET @groupby=''
+ 		IF @commands not like '%-NOGROUPBY%' OR @commands like  '%-SUMFIELDS%' 
+            BEGIN SET @groupby=concat(' GROUP BY ',@group_by) END 
+        ELSE SET @groupby=''
 
 		SET @sqlCommand = 'INSERT INTO '+ @fact_day 
         +' ([date],activity_id,forecast_id,import_id,' + @fields_target + ',site_id)' + 
         'SELECT ' + @group_by + ',' +  convert(nvarchar(max),@activity_id)  
    		+ ', ' +  convert(nvarchar(max),@forecast_id) + ','+ convert(nvarchar(max),@import_id)
    		+ ',' + @fields_source +','+ convert(nvarchar(max),@site_id) +
- 		' FROM '+ @source +' WHERE ' + @filter  
+ 		' FROM '+ @day_source +' WHERE ' + @filter  
 		+ ' AND ' + @group_by + ' BETWEEN ''' + convert(char(10),@date_import_from,126)  + ''' AND ''' + convert(char(10),@date_import_until,126) + '''' + @groupby +';'
             
  		BEGIN TRY
@@ -201,18 +196,18 @@ BEGIN
                 IF @commands like '%-LOG_INSERT%' EXEC dbo.[A_SP_SYS_LOG] 'LOG INSERT ROWS' ,@session_id ,@import_id ,'INSERT QUERY DAY',@sqlCommand 
                 SET @output=@output+'day records inserted ' + convert(varchar(10),@rows)+'<br><br>'  
             END
-END TRY
+        END TRY
    		BEGIN CATCH  
    			SET @data=JSON_MODIFY( @data,'$.error',dbo.[A_FN_SYS_ErrorJson]()) 
             SET @output=@output+dbo.[A_FN_SYS_ErrorJson]()+'<br><br>'
 			EXEC dbo.[A_SP_SYS_LOG] 'IMPORT ERROR' ,@session_id ,@import_id ,'INSERT DAY',@sqlCommand
-	END CATCH;   
+        END CATCH;   
 
 	IF @commands  like '%-INTRADAY%' AND @commands not like '%-NOINTRADAY%' BEGIN  
-
 		SET @intraday_join= case when @p1>'' then @p1 else @intraday_join end
 		SET @intraday_interval_id = case when @p1>'' then 'I.interval_id' else @intraday_interval_id end -- p1 has a join with interval table with alias I , we need override default
 		SET @intraday_duration = case when @p2>'' then @p2 else @intraday_duration end  -- standard duration of the interval is 15 min
+        SET @intraday_source= case when @source>'' then @source + ' ' + @intraday_join  else @fact_intraday end
 	----------------------------------------------------------------------------------------------------------------------
 	--  CLEAN INTRADAY
 	--------------------------------------------------------------------------------	
@@ -238,7 +233,7 @@ END TRY
  		END TRY
  		BEGIN CATCH  
 			SET @data=JSON_MODIFY( @data,'$.error',dbo.[A_FN_SYS_ErrorJson]()) 
-            SET @output=@output+dbo.[A_FN_SYS_ErrorJson]()+'<br><br>'
+            SET @output=@output + dbo.[A_FN_SYS_ErrorJson]()+'<br><br>'
 			EXEC dbo.[A_SP_SYS_LOG] 'IMPORT ERROR' ,@session_id ,@import_id ,'CLEAN INTRADAY',@sqlCommand  
  		END CATCH;   
 
@@ -246,15 +241,16 @@ END TRY
 	--  INSERT TO INTRADAY
 	-------------------------------------------------------------------------------------
  		IF @commands not like '%-NOGROUPBY%' OR @commands like  '%-SUMFIELDS%' BEGIN SET @groupby = concat(' GROUP BY ', @group_by, ',',@intraday_interval_id) END ELSE SET @groupby=''
-
+        
 		SET @sqlCommand = 'INSERT INTO '+ @fact_intraday 
         +' ([date], activity_id, forecast_id, import_id, interval_id, duration_min,' + @fields_target + ',site_id)'
         +' SELECT ' + @group_by + ',' +  convert(nvarchar(max),@activity_id)  
    		+ ',' +  convert(nvarchar(max),@forecast_id) + ','+ convert(nvarchar(max),@import_id)
-   		+ ',' + @intraday_interval_id+ ', '+ convert(varchar(5),@intraday_duration) + ',' + @fields_source 
+   		+ ',' + @intraday_interval_id + ', '+ convert(varchar(5),@intraday_duration) + ',' + @fields_source 
         + ',' + convert(nvarchar(max),@site_id) 
- 		+ ' FROM '+ @source + ' ' + @intraday_join + ' WHERE ' + @filter  
-		+ ' AND ' + @group_by + ' BETWEEN ''' + convert(char(10),@date_import_from,126)  + ''' AND ''' + convert(char(10),@date_import_until,126) + '''' + @groupby +';'
+ 		+ ' FROM '+ @intraday_source + ' WHERE ' + @filter  
+		+ ' AND ' + @group_by + ' BETWEEN ''' + convert(char(10),@date_import_from,126)  + ''' AND ''' + convert(char(10),@date_import_until,126) + '''' 
+        + @groupby +';'
             
  		BEGIN TRY
 			IF @commands like '%-PRINT%' PRINT @sqlCommand 
@@ -271,47 +267,43 @@ END TRY
         END TRY
    		BEGIN CATCH  
    			SET @data=JSON_MODIFY( @data,'$.error',dbo.[A_FN_SYS_ErrorJson]()) 
-            SET @output=@output+dbo.[A_FN_SYS_ErrorJson]()+'<br><br>'
+            SET @output=@output + dbo.[A_FN_SYS_ErrorJson]()+'<br><br>'
 			EXEC dbo.[A_SP_SYS_LOG] 'IMPORT ERROR' ,@session_id ,@import_id ,'INSERT DAY',@sqlCommand
-	END CATCH;   
-
+	    END CATCH;   
 
 	END -- end if intraday
 
-	 
-	  
+ 	FETCH NEXT FROM TAB_CURSOR 
+ 	INTO @import_id 
+ 		,@activity_id
+     	,@forecast_id
+     	,@p1
+     	,@p2
+     	,@p3
+     	,@p4
+     	,@p5
+     	,@date_import_from
+     	,@date_import_until
+     	,@fields_source
+     	,@fields_target
+     	,@schedule
+     	,@filter
+     	,@source
+     	,@group_by
+        ,@commands 
+        ,@procedure_name 
+        ,@site_id
 
- 		FETCH NEXT FROM TAB_CURSOR 
- 		INTO @import_id 
- 			,@activity_id
-     		,@forecast_id
-     		,@p1
-     		,@p2
-     		,@p3
-     		,@p4
-     		,@p5
-     		,@date_import_from
-     		,@date_import_until
-     		,@fields_source
-     		,@fields_target
-     		,@schedule
-     		,@filter
-     		,@source
-     		,@group_by
-			,@commands 
-			,@procedure_name 
-			,@site_id
-
-   		END -- END OF FETCHING IMPORTS
+   	END -- END OF FETCHING IMPORTS
 
 	CLOSE TAB_CURSOR 
 	DEALLOCATE TAB_CURSOR
 
 	SET @data=DATEDIFF(second,@start_time,getdate())
-	EXEC dbo.[A_SP_SYS_LOG] 'PROCEDURE FINISH' ,@session_id  ,null  , @procedure_name , @data
+	EXEC dbo.[A_SP_SYS_LOG] 'PROCEDURE FINISH' ,@session_id  ,'Duration sec' , @procedure_name , @data
 
     IF @commands like '%-OUTPUT%'  select @output as SQL_OUTPUT
 
 END
 
-
+GO
