aa7b37856a2f0357c222506840192f00edd822e9
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2022-07-19 11:54:27 +0200

    Update A_FACT_INTRADAY.sql

--- 01.data/A_FACT_INTRADAY.sql
+++ 01.data/A_FACT_INTRADAY.sql
@@ -1,31 +1,26 @@
-/****** Object:  Table [A_FACT_INTRADAY]    Script Date: 21-2-2022 10:34:50 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
-CREATE TABLE [A_FACT_INTRADAY](
-	[date] [datetime] NULL,
-	[interval_id] [smallint] NULL,
-	[activity_id] [int] NULL,
-	[forecast_id] [int] NULL,
-	[value1] [real] NULL,
-	[value2] [real] NULL,
-	[value3] [real] NULL,
-	[value4] [real] SPARSE  NULL,
-	[value5] [real] SPARSE  NULL,
-	[value6] [real] SPARSE  NULL,
-	[value7] [real] SPARSE  NULL,
-	[value8] [real] SPARSE  NULL,
-	[value9] [real] SPARSE  NULL,
-	[value10] [real] SPARSE  NULL,
-	[duration_min] [tinyint] NULL,
-	[import_id] [int] NULL,
-	[date_updated] [datetime] NULL,
-	[site_id] [int] NULL
+CREATE TABLE [dbo].[A_FACT_INTRADAY]
+(
+    [date] [date] NOT NULL,
+    [interval_id] [smallint] NOT NULL,
+    [activity_id] [int] NOT NULL,
+    [forecast_id] [int] NOT NULL,
+    [value1] [real] NULL,
+    [value2] [real] NULL,
+    [value3] [real] NULL,
+    [value4] [real] SPARSE NULL,
+    [value5] [real] SPARSE NULL,
+    [value6] [real] SPARSE NULL,
+    [value7] [real] SPARSE NULL,
+    [value8] [real] SPARSE NULL,
+    [value9] [real] SPARSE NULL,
+    [value10] [real] SPARSE NULL,
+    [duration_min] [tinyint] NULL,
+    [import_id] [int] NULL,
+    [date_updated] [datetime] NULL,
+    [site_id] [int] NOT NULL
 ) ON [PRIMARY]
 GO
-
-
-
