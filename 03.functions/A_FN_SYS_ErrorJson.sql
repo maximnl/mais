b99cd22d5e0ccc23811f23602fbc5667b598ad66
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2022-12-19 17:51:55 +0100

    Create A_FN_SYS_ErrorJson.sql

---
+++ 03.functions/A_FN_SYS_ErrorJson.sql
@@ -0,0 +1,28 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:      PLANSIS
+-- Create Date: 2021-01-01
+-- Description: Get error in json format
+-- =============================================
+CREATE FUNCTION [dbo].[A_FN_SYS_ErrorJson]
+( 
+)
+RETURNS nvarchar(2000)
+AS
+BEGIN
+    -- Declare the return variable here
+    DECLARE @data nvarchar(2000)
+
+		set  @data='{}'
+		set  @data=JSON_MODIFY( @data,'$.Severity',CONVERT(varchar(5), ERROR_SEVERITY()))
+		set  @data=JSON_MODIFY( @data,'$.State',CONVERT(varchar(5), ERROR_STATE()))
+		set  @data=JSON_MODIFY( @data,'$.Procedure',ISNULL(ERROR_PROCEDURE(), '-'))
+		set  @data=JSON_MODIFY( @data,'$.Line',CONVERT(varchar(5), ERROR_LINE()))
+		set  @data=JSON_MODIFY( @data,'$.Message',ERROR_MESSAGE())
+
+    RETURN @data
+END
+GO
