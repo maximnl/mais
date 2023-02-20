b3f2f8ae7bf9a3cbd4bd8eecb5a5db0a69ca9fa3
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2023-02-20 12:31:56 +0100

    Create A_SOURCE_SQLAGENT_LOG.sql
    initial

---
+++ 02.views/A_SOURCE_SQLAGENT_LOG.sql
@@ -0,0 +1,39 @@
+
+/****** Object:  View [dbo].[A_SOURCE_SQLAGENT_LOG]    Script Date: 20-2-2023 12:29:26 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+CREATE view [dbo].[A_SOURCE_SQLAGENT_LOG] AS
+SELECT 
+  convert(varchar(100),sj.name) JobName
+, sjh.step_id
+, convert(varchar(150),'step ' + right(convert(varchar(10),1000+sjh.step_id),3)+'. '+ ISNULL(sjs.step_name, 'Job Status')) StepName
+, convert(date,[msdb].dbo.agent_datetime(sjh.run_date, sjh.run_time)) RunDate
+, [msdb].dbo.agent_datetime(sjh.run_date, sjh.run_time) RunDateAndTime
+, STUFF(STUFF(RIGHT('00000' + CAST(run_duration AS VARCHAR(6)),6),3,0,':'),6,0,':')  duration_time
+, (DATEDIFF(SECOND, '0:00:00', STUFF(STUFF(RIGHT('00000' + CAST(run_duration AS VARCHAR(6)),6),3,0,':'),6,0,':')  )) duration_sec
+, CASE sjh.run_status
+    WHEN 0 THEN 'Failed'
+    WHEN 1 THEN 'Succeeded'
+    WHEN 2 THEN 'Retry'
+    WHEN 3 THEN 'Canceled'
+    WHEN 4 THEN 'In Progress'
+  END RunStatus
+, convert(varchar(4000),convert(varchar(10),(DATEDIFF(SECOND, '0:00:00', STUFF(STUFF(RIGHT('00000' + CAST(run_duration AS VARCHAR(6)),6),3,0,':'),6,0,':'))  )) + ' sec duration. ' + sjh.message) as message
+, datediff(SECOND, [msdb].dbo.agent_datetime(sjh.run_date, sjh.run_time),getdate() ) sec_ago
+, 1 as site_id
+FROM [msdb].dbo.sysjobs sj
+  INNER JOIN [msdb].dbo.sysjobhistory sjh ON sj.job_id = sjh.job_id
+  LEFT OUTER JOIN [msdb].dbo.sysjobsteps sjs ON sjh.job_id = sjs.job_id AND sjh.step_id = sjs.step_id  
+WHERE sj.name = 'NAME OF YOUR SQL AGENT JOB' 
+-- and sjh.step_name in ( 'A STEP NAME1','A STEP NAME1')
+ 
+GO
+
+
